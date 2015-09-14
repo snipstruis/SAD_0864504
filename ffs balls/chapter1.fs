4506482911d27cc49b3a6f45c37cbbbe7ef4bec7
snipstruis <kipstad@hotmail.com>
2015-09-14 17:47:02 +0200

    woo

--- ffs balls/chapter1.fs
+++ ffs balls/chapter1.fs
@@ -15,29 +15,28 @@
         let foo = b.Y+b.DY+g 
         if (b.Y+b.DY+g > 30.0) || (b.Y+b.DY+g < 0.0)
         then dt*(-b.DY)+g else b.DY+g
-      { X = b.X + dx
-        Y = b.Y + dy
+      let x = b.X + dx
+      let y = b.Y + dy
+      { X = x
+        Y = y
         DX= dx
         DY= dy
       }
 
-    let print_scene (x,y) =
+    let print_scene (balls:ball List) =
+      let high (x:int) (y:int) :(ball->bool) = function (b:ball) -> (int b.X,int b.Y) = (x,y)
       do Console.Clear()
-      let x,y = int x, int y
       for j = 10 downto 0 do
         for i = 0 to 30 do
-          if (y+1) = j && i = (x+1) then
-            Console.Write("b")
-          elif j = 0 || i = 0 || j = 10 || i = 30 then
+          if j = 0 || i = 0 || j = 10 || i = 30 then
             Console.Write("*")
           else
-            Console.Write(" ")
+            match List.tryFind (high (i-1) (j-1)) balls with
+            | None -> Console.Write(" ")
+            | _    -> Console.Write("b")
         Console.Write("\n")
       ignore(Console.ReadKey())
+      balls
 
     let rec simulate_ball (bs:ball List) :ball List=
-      let f (b:ball) :ball = 
-        let bb = b |> simulate_ball_step
-        do print_scene (bb.X,bb.Y)
-        bb
-      bs |> List.map f |> simulate_ball
+      bs |> List.map simulate_ball_step |> print_scene |> simulate_ball
