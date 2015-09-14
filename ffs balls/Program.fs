5b49fef1487168647f5f076b9d0c13b64f8c55b3
snipstruis <kipstad@hotmail.com>
2015-09-14 17:10:33 +0200

    no print yet

--- ffs balls/Program.fs
+++ ffs balls/Program.fs
@@ -1,4 +1,7 @@
 ﻿[<EntryPoint>]
 let main argv = 
-  do Chapter1.BallSimulation.simulation()
+  //do Chapter1.BallSimulation.simulation()
+  let bs :Chapter1.ball List = [{X= 5.0; Y= 8.0; DX= 0.0; DY= 0.0;}]
+  do Chapter1.BallSimulation.simulate_ball bs |> ignore
+  //do Chapter1.BallSimulation.print_scene (3.0,3.0)  |> ignore
   0
--- ffs balls/chapter1.fs
+++ ffs balls/chapter1.fs
@@ -1,22 +1,32 @@
 ﻿namespace Chapter1
+  type ball = {X:float; Y:float; DX:float; DY:float}
   module BallSimulation =
     open System
-    let dt = 0.1
-    let g = -9.81
+    let dt = 0.9
+    let g = -0.981
+    let max_x = 30.0
+    let max_y = 10.0
     
-    let simulation_step (y,v) =
-      let y',v' = (y+v*dt, v+g*dt)
-      if y' < 0.0 then
-        (0.0, -v'*0.7)
-      else
-            (y',v')
-    
-    let print_scene (y,v) =
+    let simulate_ball_step (b:ball) :ball =
+      let dx = 
+        if (b.X+b.DX > 15.0) || (b.X+b.DX < -15.0)
+        then dt*(-b.DX) else b.DX
+      let dy = 
+        let foo = b.Y+b.DY+g 
+        if (b.Y+b.DY+g > 30.0) || (b.Y+b.DY+g < 0.0)
+        then dt*(-b.DY)+g else b.DY+g
+      { X = b.X + dx
+        Y = b.Y + dy
+        DX= dx
+        DY= dy
+      }
+
+    let print_scene (x,y) =
       do Console.Clear()
-      let y,v = int y, int v
+      let x,y = int x, int y
       for j = 10 downto 0 do
         for i = 0 to 30 do
-          if (y+1) = j && i = 15 then
+          if (y+1) = j && i = (x+1) then
             Console.Write("b")
           elif j = 0 || i = 0 || j = 10 || i = 30 then
             Console.Write("*")
@@ -25,10 +35,9 @@
         Console.Write("\n")
       ignore(Console.ReadKey())
 
-    let simulation () =
-      let rec simulation (y,v) =
-        do print_scene (y,v)
-        let y',v' = simulation_step (y,v)
-        if abs v' > 0.1 then
-          do simulation (y',v')
-      do simulation (5.0,-2.0)
+    let rec simulate_ball (bs:ball List) :ball List=
+      let f (b:ball) :ball = 
+        let bb = b |> simulate_ball_step
+        do print_scene (bb.X,bb.Y)
+        bb
+      bs |> List.map f |> simulate_ball
